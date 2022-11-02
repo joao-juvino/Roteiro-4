a42793a56b0abcf925bd9b3d3d7b3d5ff4ae26a6
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2022-11-02 17:47:27 -0300

    Update code

--- src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
+++ src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
@@ -1,43 +1,43 @@
 package orderStatistic;
 
+import util.Util;
+
 /**
- * Uma implementacao da interface KLargest que usa estatisticas de ordem para 
+ * Uma implementacao da interface KLargest que usa estatisticas de ordem para
  * retornar um array com os k maiores elementos de um conjunto de dados/array.
  * 
  * A k-esima estatistica de ordem de um conjunto de dados eh o k-esimo menor
- * elemento desse conjunto. Por exemplo, considere o array [4,8,6,9,12,1]. 
- * A 3a estatistica de ordem eh 6, a 6a estatistica de ordem eh 12.
+ * elemento desse conjunto. Por exemplo, considere o array [4,8,6,9,12,1]. A 3a
+ * estatistica de ordem eh 6, a 6a estatistica de ordem eh 12.
  * 
- * Note que, para selecionar os k maiores elementos, pode-se pegar todas as 
- * estatisticas de ordem maiores que k. 
+ * Note que, para selecionar os k maiores elementos, pode-se pegar todas as
+ * estatisticas de ordem maiores que k.
  * 
- * Requisitos do algoritmo:
- * - DEVE ser in-place. Porem, voce pode modificar o array original.
- *   Voce pode criar ainda um array de tamanho k que vai armazenar apenas
- *   os elementos a serem retornados.
- * - Voce DEVE usar alguma ideia de algoritmo de ordenacao visto em sala 
- *   para calcular estatisticas de ordem.
- * - Se a entrada for invalida, deve-se retornar um array vazio (por exemplo,
- *   ao solicitar os 5 maiores elementos em um array que soh contem 3 elementos).
- *   Este metodo NUNCA deve retornar null.
+ * Requisitos do algoritmo: - DEVE ser in-place. Porem, voce pode modificar o
+ * array original. Voce pode criar ainda um array de tamanho k que vai armazenar
+ * apenas os elementos a serem retornados. - Voce DEVE usar alguma ideia de
+ * algoritmo de ordenacao visto em sala para calcular estatisticas de ordem. -
+ * Se a entrada for invalida, deve-se retornar um array vazio (por exemplo, ao
+ * solicitar os 5 maiores elementos em um array que soh contem 3 elementos).
+ * Este metodo NUNCA deve retornar null.
  * 
  * @author campelo and adalberto
  *
  * @param <T>
  */
-public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLargest<T>{
+public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLargest<T> {
 
 	@Override
 	public T[] getKLargest(T[] array, int k) {
 		// TODO Implement your code here
 		throw new UnsupportedOperationException("Not implemented yet!");
-		//este metodo deve obrigatoriamente usar o orderStatistics abaixo.
+		// este metodo deve obrigatoriamente usar o orderStatistics abaixo.
 	}
 
 	/**
-	 * Metodo que retorna a k-esima estatistica de ordem de um array, usando
-	 * a ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista 
-	 * a k-esima estatistica de ordem, entao retorna null.
+	 * Metodo que retorna a k-esima estatistica de ordem de um array, usando a ideia
+	 * de algum algoritmo de ordenacao visto em sala. Caso nao exista a k-esima
+	 * estatistica de ordem, entao retorna null.
 	 * 
 	 * Obs: o valor de k deve ser entre 1 e N.
 	 * 
@@ -45,8 +45,9 @@ public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLa
 	 * @param k
 	 * @return
 	 */
-	public T orderStatistics(T[] array, int k){
+	public T orderStatistics(T[] array, int k) {
 		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");		
+		throw new UnsupportedOperationException("Not implemented yet!");
 	}
+
 }
